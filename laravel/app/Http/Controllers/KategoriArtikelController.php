5e62975395bc060ab8088cd3a2aedac7271badb5
putri azizah <putriazizah500@gmail.com>
2019-09-28 16:30:09 +0700

    push

---
+++ laravel/app/Http/Controllers/KategoriArtikelController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\KategoriArtikel;
+
+class KategoriArtikelController extends Controller
+{
+    public function index(){
+    	//Eloquent => ORM (Object Relational Mapping)
+    	$listKategoriArtikel=KategoriArtikel::all(); //select * from kategori_artikel
+
+    	//blade
+    	return view('kategori_artikel.index',compact('listKategoriArtikel'));
+    	//return view( view: 'kategori_artikel.index')->with('data',$listKategoriArtikel);
+    }
+
+    public function show($id){
+    	//Eloquent
+    	//$KategoriArtikel=KategoriArtikel::where('id',$id)->first(); // select * from kategori_artikel where id=$id limit I
+    	$KategoriArtikel=KategoriArtikel::find($id);
+
+    	return view('kategori_artikel.show',compact('KategoriArtikel'));
+    }
+}
